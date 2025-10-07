44c6f61b5a1e7935acdee67d2021970b54929bd0
JojoSlice <johannes152@hotmail.com>
2025-10-07 16:54:25 +0200

    nvim update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -35,3 +35,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		})
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "help", "qf", "spectre_panel" },
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = event.buf, silent = true, desc = "Close window" })
+	end,
+})
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -134,6 +134,55 @@ return {
 		"nvim-pack/nvim-spectre",
 		build = false,
 		cmd = "Spectre",
-		opts = {},
+		opts = {
+			replace_engine = {
+				["sed"] = {
+					cmd = "sed",
+					args = nil,
+				},
+			},
+			default = {
+				find = {
+					cmd = "rg",
+					options = { "ignore-case" },
+				},
+				replace = {
+					cmd = "sed",
+				},
+			},
+			is_insert_mode = false,
+			open_cmd = "noswapfile vnew",
+		},
+		keys = {
+			{
+				"<leader>S",
+				function()
+					require("spectre").toggle()
+				end,
+				desc = "Toggle Spectre",
+			},
+			{
+				"<leader>sw",
+				function()
+					require("spectre").open_visual({ select_word = true })
+				end,
+				desc = "Search current word",
+			},
+			{
+				"<leader>sw",
+				function()
+					require("spectre").open_visual()
+				end,
+				mode = "v",
+				desc = "Search selection",
+			},
+			{
+				"<leader>sf",
+				function()
+					require("spectre").open_file_search({ select_word = true })
+				end,
+				desc = "Search in current file",
+			},
+		},
 	},
 }
