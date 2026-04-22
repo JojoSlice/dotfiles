f9dd8cba15bded3008946a5eb8ad6ce397d9fb40
Johannes Lindell <johannes152@hotmail.com>
2026-04-22 10:34:11 +0200

    mini icons

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -96,7 +96,7 @@ return {
 
 	{
 		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = { "nvim-tree/nvim-web-devicons", { "echasnovski/mini.icons", opts = {} } },
 		lazy = false,
 		opts = {
 			default_file_explorer = true,
